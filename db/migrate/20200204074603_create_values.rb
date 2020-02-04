fbbf3c873d49c4bd1e91eecbb882005aea832dd4
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2020-02-04 17:33:12 +0900

    Execute Migration File

--- db/migrate/20200204074603_create_values.rb
+++ db/migrate/20200204074603_create_values.rb
@@ -1,8 +1,8 @@
 class CreateValues < ActiveRecord::Migration[5.2]
   def change
     create_table :values do |t|
-      t.bigint(20) :dealing_id, null: true, foreign_key: true
-      t.bigint(20) :user_id, null: true, foreign_key: true
+      t.integer :dealing_id, null: true, foreign_key: true, limit: 6
+      t.integer :user_id, null: true, foreign_key: true, limit: 6
       t.datetime :created_at, null: false
       t.datetime :updated_at, null: false
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_064714) do
+ActiveRecord::Schema.define(version: 2020_02_04_074603) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2020_02_04_064714) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "values", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "dealing_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
