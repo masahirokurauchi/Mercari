275f3a7e8bd2913e36d83967780fbd92e2931bba
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2020-02-04 18:06:50 +0900

    Execute Migration File

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_084800) do
+ActiveRecord::Schema.define(version: 2020_02_04_085208) do
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "size_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
