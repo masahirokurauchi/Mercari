baf6784fad52f56e972bb9586f80dac751d5ff45
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2020-02-06 15:51:03 +0900

    Execute Migration File

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_063649) do
+ActiveRecord::Schema.define(version: 2020_02_06_064200) do
+
+  create_table "category_sizegroups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "size_group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comment_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
