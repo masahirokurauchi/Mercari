1742cc00b713a6bdff8399f28dc958f046bb73ab
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2020-02-07 14:31:34 +0900

    Execute Migration File

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_085645) do
+ActiveRecord::Schema.define(version: 2020_02_07_052059) do
+
+  create_table "brand_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "brand_names", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
