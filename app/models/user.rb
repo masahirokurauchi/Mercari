64393e93f8cef1b34998f13b73d5e4969f76197d
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2020-02-17 14:28:59 +0900

    Set up Association and Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  #Association
+  has_one :address, dependent: :destroy
+
+  #Validation
+  validates :nickname, :birthday, :earnings, :points, presence: true
+  validates :first_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  validates :last_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
 end
