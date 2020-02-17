32856c0a6d4907b6b87022988a9f7eb5f6b37d65
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2020-02-17 16:29:50 +0900

    Set up Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "items#index"
+
+  devise_scope :user do
+    ## ↓登録方法の選択ページ
+    get 'users/select_registration', to: 'users/registrations#select', as: :select_registration
+  end
 
   # devise_for :users
+  # root to: "items#index"
 
-  resources :users, only: [:show] do
-    collection do
-      get "card"
-      get "selling"
-      get "selling_progress"
-      get "sold"
-      get "bought_progress"
-      get "bought_past"
-    end
-  end
 
-  resources :items  do
-    member do
-      get "purchase_confirmation"
-      post "purchase"
-    end
-  end
-  resources :categories, only: [:index, :show]
-  resource :cards, only: [:new, :create, :show, :update, :destroy]
+  # resources :users, only: [:show] do
+  #   collection do
+  #     get "card"
+  #     get "selling"
+  #     get "selling_progress"
+  #     get "sold"
+  #     get "bought_progress"
+  #     get "bought_past"
+  #   end
+  # end
+
+  # resources :items  do
+  #   member do
+  #     get "purchase_confirmation"
+  #     post "purchase"
+  #   end
+  # end
+  # resources :categories, only: [:index, :show]
+  # resource :cards, only: [:new, :create, :show, :update, :destroy]
 
 end
