bdba422cccab57fe013122e4d3a913271134fafb
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2020-03-07 15:47:48 +0900

    Define Show Action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   #to use no_menu layout
-  layout "no_menu"
+  # layout "no_menu"
 
   def index
     return false if Item.count == 0 ## 商品数がゼロのときはランキングが作れないのでここで終了
@@ -10,16 +10,22 @@ class ItemsController < ApplicationController
     @categories = @categories.map{|category_name| Categorie.find_by(name: category_name)} ## カテゴリの名前たちを使ってカテゴリのインスタンスが入った配列を作成
     @categories.each do |category|
       @new_items_arrays << Item.search_by_category(category.subtree_ids).order("created_at DESC").limit(4) ## カテゴリごとの新着アイテムを配列化する
-      
+
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new ## 出品ページ
+    render layout: "no_menu"
     @item = Item.new
     @item.item_images.build  ## 新規画像用
   end
 
   def edit ## 商品編集ページ
+    render layout: "no_menu"
     @item = current_user.items.find(params[:id])
     @item.item_images.build  ## 新規画像用
   end
