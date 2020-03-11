94f5c3034d2f3a3fa2d4da2fa6835fcf647a8219
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2020-03-11 16:13:21 +0900

    Specify Association with Item

--- app/models/dealing.rb
+++ app/models/dealing.rb
@@ -1,5 +1,5 @@
 class Dealing < ApplicationRecord
     # Association
 	belongs_to :user, class_name: 'User', foreign_key: 'buyer_id'
-	belongs_to :item
+	belongs_to :item, class_name: 'Item', foreign_key: 'item_id'
 end
